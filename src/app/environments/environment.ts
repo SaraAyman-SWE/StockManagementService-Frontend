de660f28c235b262f373a70bc891f16a5cffbc92
Sara Ayman <saraayman20@gmail.com>
2024-05-05 04:24:43 +0300

    Update environment.ts

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -2,6 +2,6 @@ export const environment = {
     production: false,
     title: 'Local Environment Heading',
     apiURL: 'https://localhost:44300',
-    authenticationUsername: 'guest',
-    authenticationPassword: 'guest'
+    authenticationUsername: '',
+    authenticationPassword: ''
   };
